e85e171c9a40d95170c53bae5cc19d16b74f8568
Zehafi Kettabi Anass <139884348+anas20s@users.noreply.github.com>
2023-09-18 22:33:34 +0200

    Update connexionBaseDeDonnées.php

--- includes/connexionBaseDeDonnées.php
+++ includes/connexionBaseDeDonnées.php
@@ -7,23 +7,16 @@
 </head>
 <body>
 <?php
-class Database {
-    private $host = "localhost";
-    private $db_name = "ecfgaragestudi";
-    private $username = "root";
-    private $password = "";
-    public $conn;
-
-    public function getConnection(){
-        $this->conn = null;
-        try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }catch(PDOException $exception){
-            echo "Erreur de connexion : " . $exception->getMessage();
-        }
-        return $this->conn;
-    }
+$servername = 'localhost';
+$username = 'root';
+$password = ''; 
+$dbname = 'ecfgaragestudi';
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Connexion réussie';
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
 }
 ?>
 
