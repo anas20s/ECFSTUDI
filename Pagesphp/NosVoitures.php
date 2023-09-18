8d3f4fbbfea9421525d6a889ac006f05d168c1f5
Zehafi Kettabi Anass <139884348+anas20s@users.noreply.github.com>
2023-09-18 22:48:22 +0200

    Update NosVoitures.php

--- Pagesphp/NosVoitures.php
+++ Pagesphp/NosVoitures.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nos voitures en vente</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
 <body>
     <?php include 'header.php'; ?>
     <!--presentation des voitures en vente-->
